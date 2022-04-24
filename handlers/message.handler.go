27fc749d983e11304c217ed2e8d726e89419329f
mystique09 <knightkrypto7@gmail.com>
2022-04-25 01:49:02 +0800

    feat: CRUD for message

--- handlers/message.handler.go
+++ handlers/message.handler.go
@@ -2,26 +2,49 @@ package handlers
 
 import (
 	"confessit/models"
+	"time"
 
 	"gorm.io/gorm"
 )
 
-func GetMessages(conn *gorm.DB) []models.Message {
-	return make([]models.Message, 2)
+var MMessage = models.Message{}
+
+func GetMessages(conn *gorm.DB, uname string) []models.Message {
+	var messages []models.Message
+
+	conn.Model(&MMessage).Where("to = ?", uname).Find(&messages)
+
+	return messages
 }
 
-func GetMessage(conn *gorm.DB, name string) models.Message {
-	return models.Message{}
+func GetMessage(conn *gorm.DB, uname string) models.Message {
+	var message models.Message
+
+	conn.Model(&MMessage).Where("to = ?", uname).Find(&message)
+
+	return message
 }
 
-func CreateMessage(conn *gorm.DB, payload models.MessageCreatePayload) models.Message {
-	return models.Message{}
+func CreateMessage(conn *gorm.DB, payload models.MessageCreatePayload) error {
+	var total_message = GetMessages(conn, payload.To)
+	var nmessage models.Message = models.Message{
+		ID:        uint32(len(total_message) + 1),
+		To:        payload.To,
+		Message:   payload.Message,
+		CreatedAt: time.Now(),
+	}
+
+	if err := conn.Create(&nmessage).Error; err != nil {
+		return err
+	}
+
+	return nil
 }
 
-func UpdateMessage(conn *gorm.DB, uid uint32, payload models.UserUpdatePayload) bool {
-	return false
-}
+func DeleteMessage(conn *gorm.DB, uid uint32) error {
+	if err := conn.Delete(&MMessage, "id = ?", uid).Error; err != nil {
+		return err
+	}
 
-func DeleteMessage(conn *gorm.DB, uid uint32) bool {
-	return false
+	return nil
 }
