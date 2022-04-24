57927452994531b7ee5d37c88d6c0b4b155b0a98
mystique09 <knightkrypto7@gmail.com>
2022-04-25 01:50:07 +0800

    feat: Route struct to organize routers

--- routers/index.route.go
+++ routers/index.route.go
@@ -4,7 +4,12 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"gorm.io/gorm"
 )
+
+type Route struct {
+	Conn *gorm.DB
+}
 
 func ConfessIt(c echo.Context) error {
 	return c.String(http.StatusOK, "Confess anonymously!")
