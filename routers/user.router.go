9e4ee17949b6095d1c49c6aae892c540183e615e
mystique09 <knightkrypto7@gmail.com>
2022-04-25 01:50:49 +0800

    add: starter routes for users

--- routers/user.router.go
+++ routers/user.router.go
@@ -1,1 +1,23 @@
 package routers
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+func (r *Route) GetUsers(c echo.Context) error {
+	return c.String(http.StatusOK, "Get users")
+}
+
+func (r *Route) GetUser(c echo.Context) error {
+	return c.String(http.StatusOK, "Get user")
+}
+
+func (r *Route) UpdateUser(c echo.Context) error {
+	return c.String(http.StatusOK, "Update user")
+}
+
+func (r *Route) DeleteUser(c echo.Context) error {
+	return c.String(http.StatusOK, "Delete user")
+}
