450c358ce264f9c7f1fc144e8d4c7639440b4b86
mystique09 <knightkrypto7@gmail.com>
2022-04-26 00:08:31 +0800

    feat: MODE env variable

--- utils/index.util.go
+++ utils/index.util.go
@@ -2,16 +2,19 @@ package utils
 
 import (
 	"net/http"
+	"os"
 )
 
 func CreateCookie(name string, value string, maxAge int) http.Cookie {
+	var mode string = os.Getenv("MODE")
+
 	new_cookie := new(http.Cookie)
 	new_cookie.Name = name
 	new_cookie.Value = value
 	new_cookie.MaxAge = maxAge
 	new_cookie.Path = "/"
 	new_cookie.HttpOnly = true
-	new_cookie.Secure = true
+	new_cookie.Secure = mode == "production"
 
 	return *new_cookie
 }
