0724791ca614e8f34e099e14062bd689e4884942
mystique09 <knightkrypto7@gmail.com>
2022-04-25 18:11:51 +0800

    feat: custom middlewares

--- routers/middleware.route.go
+++ routers/middleware.route.go
@@ -1,6 +1,8 @@
 package routers
 
 import (
+	"os"
+
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
@@ -11,4 +13,19 @@ func CustomLoggerMiddleware() echo.MiddlewareFunc {
 			Format: `[${time_rfc3339}] ${status} /${method} ${host}${path} ${latency_human}` + "\n",
 		},
 	)
+}
+
+func CustomCORSMiddleware() echo.MiddlewareFunc {
+	return middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{os.Getenv("FRONTEND_URL")},
+		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept},
+	})
+}
+
+func CustomCSRFMiddleware() echo.MiddlewareFunc {
+	return middleware.CSRFWithConfig(middleware.DefaultCSRFConfig)
+}
+
+func CustomRateLimitMiddleware() echo.MiddlewareFunc {
+	return middleware.RateLimiter(middleware.NewRateLimiterMemoryStore(20))
 }
