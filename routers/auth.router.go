b349a64a374cf19c866c4f54d7fb4aafd6dacadb
mystique09 <knightkrypto7@gmail.com>
2022-04-25 15:19:03 +0800

    add: signup route functionality

--- routers/auth.router.go
+++ routers/auth.router.go
@@ -1,13 +1,39 @@
 package routers
 
 import (
+	"confessit/handlers"
+	"confessit/models"
 	"net/http"
 
+	"github.com/go-playground/validator/v10"
 	"github.com/labstack/echo/v4"
 )
 
+var validate *validator.Validate = validator.New()
+
 func (r *Route) Signup(c echo.Context) error {
-	return c.String(http.StatusOK, "Signup")
+	var payload models.UserCreatePayload
+
+	if err := (&echo.DefaultBinder{}).BindBody(c, &payload); err != nil {
+		return c.JSON(http.StatusBadRequest, err.Error())
+	}
+
+	if err := validate.Struct(payload); err != nil {
+		return c.JSON(http.StatusBadRequest, err)
+	}
+
+	hasUser := handlers.GetUser(r.Conn, payload.Username)
+
+	if hasUser.Username != "" {
+		return c.JSON(http.StatusBadRequest, "user already exist.")
+
+	}
+
+	if err := handlers.CreateUser(r.Conn, payload); err != nil {
+		return c.JSON(http.StatusBadRequest, err)
+	}
+
+	return c.JSON(http.StatusCreated, "New user created.")
 }
 
 func (r *Route) Login(c echo.Context) error {
