b1b65e34699e021500ff19dc762e8157640b911c
mystique09 <knightkrypto7@gmail.com>
2022-04-25 15:18:28 +0800

    feat: password hashing in model creation

--- models/user.model.go
+++ models/user.model.go
@@ -4,6 +4,7 @@ import (
 	"time"
 
 	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type User struct {
@@ -15,14 +16,25 @@ type User struct {
 	DeletedAt time.Time `json:"deleted_at" gorm:"index"`
 }
 
+func (u *User) HashPassword() error {
+	hash_password, err := bcrypt.GenerateFromPassword([]byte(u.Password), bcrypt.DefaultCost)
+
+	if err != nil {
+		return err
+	}
+
+	u.Password = string(hash_password)
+	return nil
+}
+
 type UserCreatePayload struct {
-	Username string `json:"username"`
-	Password string `json:"password"`
+	Username string `json:"username" validate:"required,min=8,max=18"`
+	Password string `json:"password" validate:"required,numeric,min=12"`
 }
 
 type UserUpdatePayload struct {
-	Username string `json:"username"`
-	Password string `json:"password"`
+	Username string `json:"username" validate:"required,min=8,max=18"`
+	Password string `json:"password" validate:"required,numeric,min=12"`
 }
 
 type UserResponse struct {
