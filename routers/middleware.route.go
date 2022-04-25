084b6511c51a3c00af6c09a31a8802246cc9a0d6
mystique09 <knightkrypto7@gmail.com>
2022-04-25 23:39:23 +0800

    add: auth middleware

--- routers/middleware.route.go
+++ routers/middleware.route.go
@@ -1,8 +1,12 @@
 package routers
 
 import (
+	"confessit/models"
+	"confessit/utils"
+	"net/http"
 	"os"
 
+	"github.com/google/uuid"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
@@ -28,4 +32,36 @@ func CustomCSRFMiddleware() echo.MiddlewareFunc {
 
 func CustomRateLimitMiddleware() echo.MiddlewareFunc {
 	return middleware.RateLimiter(middleware.NewRateLimiterMemoryStore(20))
+}
+
+func (r *Route) AuthMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		session, err := c.Cookie("session_token")
+
+		if err != nil {
+			if err == http.ErrNoCookie {
+				return c.JSON(http.StatusUnauthorized, "no cookie in headers")
+			}
+			return c.JSON(http.StatusBadRequest, "err getting cookie in headers")
+		}
+
+		sessionToken := session.Value
+		var token models.Session
+
+		r.Conn.Model(&models.Session{}).Where("id = ?", sessionToken).Find(&token)
+
+		if token.ID == uuid.Nil {
+			return c.JSON(http.StatusUnauthorized, "Unauthorized")
+		}
+
+		if token.IsExpired() {
+			new_cookie := utils.CreateCookie("session_token", "", -1)
+			c.SetCookie(&new_cookie)
+			r.Conn.Delete(&models.Session{}, "id = ?", sessionToken)
+
+			return c.JSON(http.StatusUnauthorized, "expired cookie")
+		}
+
+		return next(c)
+	}
 }
