f18dce9b4df6e201ed1446c279b5add94fb219a3
mystique09 <knightkrypto7@gmail.com>
2022-04-26 09:56:27 +0800

    add: domain and samesite field

--- utils/index.util.go
+++ utils/index.util.go
@@ -7,12 +7,15 @@ import (
 
 func CreateCookie(name string, value string, maxAge int) http.Cookie {
 	var mode string = os.Getenv("MODE")
+	domain := os.Getenv("COOKIE_DOMAIN")
 
 	new_cookie := new(http.Cookie)
 	new_cookie.Name = name
 	new_cookie.Value = value
 	new_cookie.MaxAge = maxAge
 	new_cookie.Path = "/"
+	new_cookie.Domain = domain
+	new_cookie.SameSite = http.SameSiteNoneMode
 	new_cookie.HttpOnly = true
 	new_cookie.Secure = mode == "production"
 
