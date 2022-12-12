2ff2eb8ef373e2f0c141b7e22461f257d7db334c
mystique09 <knightkrypto7@gmail.com>
2022-12-12 16:57:04 +0800

    add: GetUserByUsername, Create, Update, and Delete

--- db/queries/user.sql
+++ db/queries/user.sql
@@ -1,7 +1,13 @@
--- name: GetUser :one
+-- name: GetUserById :one
 SELECT *
 FROM "user"
 WHERE id = $1
+LIMIT 1;
+
+-- name: GetUserByUsername :one
+SELECT *
+FROM "user"
+WHERE username = $1
 LIMIT 1;
 
 -- name: ListUsers :many
@@ -9,3 +15,21 @@ SELECT *
 FROM "user"
 LIMIT 20
 OFFSET $1;
+
+-- name: CreateUser :one
+INSERT INTO "user" (
+    id, username, password
+) VALUES (
+    $1, $2, $3
+) RETURNING *;
+
+-- name: UpdateUsername :one
+UPDATE "user"
+SET username = $1
+WHERE id = $2
+RETURNING id;
+
+-- name: DeleteOneUser :one
+DELETE FROM "user"
+WHERE id = $1
+RETURNING id;
