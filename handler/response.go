ceb8dd88132b5594851b579b1704b0f689ace7f9
mystique09 <knightkrypto7@gmail.com>
2022-12-14 00:38:24 +0800

    add: INTERNAL_ERROR

--- handler/response.go
+++ handler/response.go
@@ -3,6 +3,7 @@ package handler
 var MISSING_FIELD = newError("missing required field(s)")
 var NOT_FOUND = newError("resource not found")
 var UNAUTHORIZED = newError("you don't have the permission to perform such task")
+var INTERNAL_ERROR = newError("something went wrong, please try again later")
 var INVALID_TOKEN = newError("unable to cast token, maybe expired?")
 
 type response struct {
