dececa5b30a3693c440e9010ae1bf6b938e2065f
mystique09 <knightkrypto7@gmail.com>
2022-12-15 23:40:17 +0800

    add: documentation for messages

--- handler/message.go
+++ handler/message.go
@@ -12,13 +12,56 @@ import (
 )
 
 type (
+	// swagger:model
 	createMessageRequest struct {
+		// the receiver_id
+		// type: uuid
+		// required: true
 		ReceiverId uuid.UUID `json:"receiver_id" validate:"required"`
-		Content    string    `json:"content" validate:"required,gte=1"`
+
+		// the actual message
+		// type: string
+		// required: true
+		Content string `json:"content" validate:"required,gte=1"`
 	}
 )
 
+// creates a new message
 func (s *Server) createMessage(c echo.Context) error {
+	// Creates a new message.
+	// swagger:operation POST /messages messages createMessage
+	//
+	// ---
+	// produces:
+	// - application/json
+	// consumes:
+	// - application/json
+	//
+	// parameters:
+	// - name: body
+	//   in: body
+	//   description: this payload holds the receiver_id and the message
+	//   required: true
+	//   schema:
+	//     "$ref": "#/definitions/createMessageRequest"
+	//
+	// responses:
+	//  200:
+	//	  description: Success response with user information.
+	//	  schema:
+	//	     type: object
+	//		 	"$ref": "#/definitions/SuccessResponse"
+	//  400:
+	//	  description: Bad request.
+	//	  schema:
+	//	     type: object
+	//		 	"$ref": "#/definitions/BadRequestResponse"
+	//  500:
+	//	  description: Internal error.
+	//	  schema:
+	//	     type: object
+	//		 	"$ref": "#/definitions/InternalErrorResponse"
+
 	data := new(createMessageRequest)
 
 	if err := c.Bind(&data); err != nil {
@@ -50,12 +93,69 @@ func (s *Server) createMessage(c echo.Context) error {
 	return c.JSON(200, newResponse(message))
 }
 
+// List all messages that has sent to the user
 func (s *Server) listMessages(c echo.Context) error {
+	// List all messages that has sent to the user.
+	// swagger:operation GET /users/{id}/messages messages listMessages
+	//
+	// ---
+	// produces:
+	// - application/json
+	// consumes:
+	// - application/json
+	//
+	// parameters:
+	// - name: page
+	//   in: query
+	//   description: the page number
+	//   required: false
+	//   type: integer
+	//   format: int64
+	// - name: id
+	//   in: path
+	//   description: the user id
+	//   required: true
+	//   type: string
+	//   format: uuid
+	//
+	// security:
+	// - key: []
+	//
+	// responses:
+	//  200:
+	//	  description: Success response with user information.
+	//	  schema:
+	//	     type: object
+	//		 	"$ref": "#/definitions/SuccessResponse"
+	//  400:
+	//	  description: Bad request.
+	//	  schema:
+	//	     type: object
+	//		 	"$ref": "#/definitions/BadRequestResponse"
+	//  401:
+	//	  description: Unauthorized.
+	//	  schema:
+	//	     type: object
+	//		 	"$ref": "#/definitions/UnauthorizedResponse"
+	//  500:
+	//	  description: Internal error.
+	//	  schema:
+	//	     type: object
+	//		 	"$ref": "#/definitions/InternalErrorResponse"
 	id := c.Param("id")
 	userId, err := uuid.Parse(id)
 
 	if err != nil {
 		return c.JSON(http.StatusBadRequest, newError(err.Error()))
+	}
+
+	tokenPayload, ok := c.Get("user").(*token.Payload)
+	if !ok {
+		return c.JSON(http.StatusUnauthorized, UNAUTHORIZED)
+	}
+
+	if tokenPayload.UserId != userId {
+		return c.JSON(http.StatusUnauthorized, UNAUTHORIZED)
 	}
 
 	pageParam := c.QueryParam("page")
@@ -83,7 +183,45 @@ func (s *Server) listMessages(c echo.Context) error {
 	return c.JSON(200, newResponse(messages))
 }
 
+// Get a message by id
 func (s *Server) getMessageById(c echo.Context) error {
+	// Get a message by id.
+	// swagger:operation GET /messages/{id} messages getMessageById
+	//
+	// ---
+	// produces:
+	// - application/json
+	// consumes:
+	// - application/json
+	//
+	// parameters:
+	// - name: id
+	//   in: path
+	//   description: the message id
+	//   required: true
+	//   type: string
+	//   format: uuid
+	//
+	// security:
+	// - key: []
+	//
+	// responses:
+	//  200:
+	//	  description: Success response with user information.
+	//	  schema:
+	//	     type: object
+	//		 	"$ref": "#/definitions/SuccessResponse"
+	//  400:
+	//	  description: Bad request.
+	//	  schema:
+	//	     type: object
+	//		 	"$ref": "#/definitions/BadRequestResponse"
+	//  500:
+	//	  description: Internal error.
+	//	  schema:
+	//	     type: object
+	//		 	"$ref": "#/definitions/InternalErrorResponse"
+
 	id := c.Param("id")
 	msgId, err := uuid.Parse(id)
 	if err != nil {
