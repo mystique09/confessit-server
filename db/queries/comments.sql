19554c0ee710860b0ea15bceade6e8969938da5d
mystique09 <knightkrypto7@gmail.com>
2022-12-18 22:05:02 +0800

    add: query one comment

--- db/queries/comments.sql
+++ db/queries/comments.sql
@@ -1,6 +1,9 @@
 -- name: ListAllComments :many
 SELECT * FROM "comments" WHERE post_id = $1;
 
+-- name: GetComment :one
+SELECT * FROM "comments" WHERE id = $1;
+
 -- name: CreateComment :one
 INSERT INTO "comments" (id, content, user_identity_id, post_id, parent_id) VALUES ($1, $2, $3, $4, $5) RETURNING *;
 
