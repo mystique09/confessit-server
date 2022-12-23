e839950887436c620a3cf24876a7046fa2001fc8
mystique09 <knightkrypto7@gmail.com>
2022-12-23 23:04:43 +0800

    feat: removed

--- db/queries/comment_likes.sql
+++
@@ -1,8 +0,0 @@
--- name: ListAllCommentLikes :many
-SELECT * FROM "comment_likes" WHERE comment_id = $1;
-
--- name: CreateCommentLike :one
-INSERT INTO "comment_likes" (id, comment_id, user_identity_id, type) VALUES ($1, $2, $3, $4) RETURNING *;
-
--- name: DeleteCommentLike :one
-DELETE FROM "comment_likes" WHERE comment_id = $1 AND user_identity_id = $2 RETURNING *;
