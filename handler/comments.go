dc94b243d8ff8fb8f20d74655837b0e62583c882
mystique09 <knightkrypto7@gmail.com>
2022-12-23 23:03:50 +0800

    add: comments

--- handler/comments.go
+++ handler/comments.go
@@ -2,23 +2,60 @@ package handler
 
 import (
 	db "cnfs/db/sqlc"
+	"cnfs/token"
 	"database/sql"
 	"net/http"
+	"time"
 
 	"github.com/google/uuid"
 	"github.com/labstack/echo/v4"
 )
 
 type (
+	// swagger:model
 	createCommentRequest struct {
 		PostId         uuid.UUID `json:"post_id" validate:"required"`
 		Content        string    `json:"content" validate:"required"`
 		UserIdentityId uuid.UUID `json:"user_identity_id" validate:"required"`
-		ParentId       uuid.UUID `json:"parent_id"`
+		ParentId       uuid.UUID `json:"parent_id" validate:"required"`
+	}
+
+	// swagger:model
+	updateCommentRequest struct {
+		Content string `json:"content" validate:"required"`
 	}
 )
 
+// list all comments of a post
 func (s *Server) listAllComments(c echo.Context) error {
+	// make a swagger docs
+
+	// swagger:operation GET /posts/{id}/comments comments listAllComments
+	// ---
+	// summary: List all comments of a post
+	// description: List all comments of a post
+	// parameters:
+	// - name: id
+	//   in: path
+	//   description: post id
+	//   required: true
+	//   type: string
+	// responses:
+	//   '200':
+	//     description: OK
+	//     schema:
+	//       type: array
+	//       items:
+	//         "$ref": "#/definitions/SuccessResponse"
+	//   '404':
+	//     description: Not Found
+	//     schema:
+	//       "$ref": "#/definitions/BadRequestResponse"
+	//   '500':
+	//     description: Internal Server Error
+	//     schema:
+	//       "$ref": "#/definitions/InternalErrorResponse"
+
 	id := c.Param("id")
 	postId, err := uuid.Parse(id)
 	if err != nil {
@@ -36,7 +73,32 @@ func (s *Server) listAllComments(c echo.Context) error {
 	return c.JSON(http.StatusOK, newResponse(comments))
 }
 
+// get comment by id
 func (s *Server) getCommentById(c echo.Context) error {
+	// swagger:operation GET /comments/{id} comments getCommentById
+	// ---
+	// summary: Get comment by id
+	// description: Get comment by id
+	// parameters:
+	// - name: id
+	//   in: path
+	//   description: comment id
+	//   required: true
+	//   type: string
+	// responses:
+	//   '200':
+	//     description: OK
+	//     schema:
+	//       "$ref": "#/definitions/SuccessResponse"
+	//   '404':
+	//     description: Not Found
+	//     schema:
+	//       "$ref": "#/definitions/BadRequestResponse"
+	//   '500':
+	//     description: Internal Server Error
+	//     schema:
+	//       "$ref": "#/definitions/InternalErrorResponse"
+
 	id := c.Param("id")
 	commentId, err := uuid.Parse(id)
 	if err != nil {
@@ -54,10 +116,43 @@ func (s *Server) getCommentById(c echo.Context) error {
 	return c.JSON(http.StatusOK, newResponse(comment))
 }
 
+// create a comment
 func (s *Server) createComment(c echo.Context) error {
-	var req createCommentRequest
+	// swagger:operation POST /comments comments createComment
+	// ---
+	// summary: Create a comment
+	// description: Create a comment
+	// parameters:
+	// - name: body
+	//   in: body
+	//   description: comment
+	//   required: true
+	//   schema:
+	//     "$ref": "#/definitions/createCommentRequest"
+	// security:
+	// - key: []
+	// responses:
+	//   '200':
+	//     description: OK
+	//     schema:
+	//       "$ref": "#/definitions/SuccessResponse"
+	//   '400':
+	//     description: Bad Request
+	//     schema:
+	//       "$ref": "#/definitions/BadRequestResponse"
+	//   '500':
+	//     description: Internal Server Error
+	//     schema:
+	//       "$ref": "#/definitions/InternalErrorResponse"
+
+	req := new(createCommentRequest)
 	if err := c.Bind(&req); err != nil {
 		return c.JSON(http.StatusBadRequest, newError(err.Error()))
+	}
+
+	commentId := uuid.New()
+	if req.ParentId == uuid.Nil {
+		req.ParentId = commentId
 	}
 
 	if err := c.Validate(req); err != nil {
@@ -65,14 +160,172 @@ func (s *Server) createComment(c echo.Context) error {
 	}
 
 	comment, err := s.store.CreateComment(c.Request().Context(), db.CreateCommentParams{
-		ID:             uuid.New(),
+		ID:             commentId,
 		PostID:         req.PostId,
 		Content:        req.Content,
 		UserIdentityID: req.UserIdentityId,
-		ParentID: uuid.NullUUID{
-			UUID: req.ParentId,
-		},
+		ParentID:       req.ParentId,
+		CreatedAt:      time.Now(),
+		UpdatedAt:      time.Now(),
 	})
+
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return c.JSON(http.StatusNotFound, NOT_FOUND)
+		}
+		return c.JSON(http.StatusInternalServerError, INTERNAL_ERROR)
+	}
+
+	return c.JSON(http.StatusOK, newResponse(comment))
+}
+
+func (s *Server) updateComment(c echo.Context) error {
+	// swagger:operation PUT /comments/{id} comments updateComment
+	// ---
+	// summary: Update a comment
+	// description: Update a comment
+	// parameters:
+	// - name: id
+	//   in: path
+	//   description: comment id
+	//   required: true
+	//   type: string
+	// - name: body
+	//   in: body
+	//   description: comment
+	//   required: true
+	//   schema:
+	//     "$ref": "#/definitions/updateCommentRequest"
+	// security:
+	// - key: []
+	// responses:
+	//   '200':
+	//     description: OK
+	//     schema:
+	//       "$ref": "#/definitions/SuccessResponse"
+	//   '400':
+	//     description: Bad Request
+	//     schema:
+	//       "$ref": "#/definitions/BadRequestResponse"
+	//   '500':
+	//     description: Internal Server Error
+	//     schema:
+	//       "$ref": "#/definitions/InternalErrorResponse"
+
+	req := new(updateCommentRequest)
+	if err := c.Bind(&req); err != nil {
+		return c.JSON(http.StatusBadRequest, newError(err.Error()))
+	}
+
+	id := c.Param("id")
+	commentId, err := uuid.Parse(id)
+	if err != nil {
+		return c.JSON(400, newError(err.Error()))
+	}
+
+	if err := c.Validate(req); err != nil {
+		return c.JSON(http.StatusBadRequest, err)
+	}
+
+	tokenPayload, ok := c.Get("user").(*token.Payload)
+	if !ok {
+		return c.JSON(http.StatusUnauthorized, UNAUTHORIZED)
+	}
+
+	userIdentityId, err := s.store.GetUserIdentityByUserId(c.Request().Context(), tokenPayload.UserId)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return c.JSON(http.StatusNotFound, NOT_FOUND)
+		}
+		return c.JSON(http.StatusInternalServerError, INTERNAL_ERROR)
+	}
+
+	hasComment, err := s.store.GetComment(c.Request().Context(), commentId)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return c.JSON(http.StatusNotFound, NOT_FOUND)
+		}
+		return c.JSON(http.StatusInternalServerError, INTERNAL_ERROR)
+	}
+
+	if userIdentityId.ID != hasComment.UserIdentityID {
+		return c.JSON(http.StatusUnauthorized, UNAUTHORIZED)
+	}
+
+	comment, err := s.store.UpdateComment(c.Request().Context(), db.UpdateCommentParams{
+		ID:        commentId,
+		Content:   req.Content,
+		UpdatedAt: time.Now(),
+	})
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return c.JSON(http.StatusNotFound, NOT_FOUND)
+		}
+		return c.JSON(http.StatusInternalServerError, INTERNAL_ERROR)
+	}
+
+	return c.JSON(http.StatusOK, newResponse(comment))
+}
+
+func (s *Server) deleteComment(c echo.Context) error {
+	// swagger:operation DELETE /comments/{id} comments deleteComment
+	// ---
+	// summary: Delete a comment
+	// description: Delete a comment
+	// parameters:
+	// - name: id
+	//   in: path
+	//   description: comment id
+	//   required: true
+	//   type: string
+	// security:
+	// - key: []
+	// responses:
+	//   '200':
+	//     description: OK
+	//     schema:
+	//       "$ref": "#/definitions/SuccessResponse"
+	//   '400':
+	//     description: Bad Request
+	//     schema:
+	//       "$ref": "#/definitions/BadRequestResponse"
+	//   '500':
+	//     description: Internal Server Error
+	//     schema:
+	//       "$ref": "#/definitions/InternalErrorResponse"
+
+	id := c.Param("id")
+	commentId, err := uuid.Parse(id)
+	if err != nil {
+		return c.JSON(400, newError(err.Error()))
+	}
+
+	tokenPayload, ok := c.Get("user").(*token.Payload)
+	if !ok {
+		return c.JSON(http.StatusUnauthorized, UNAUTHORIZED)
+	}
+
+	userIdentityId, err := s.store.GetUserIdentityByUserId(c.Request().Context(), tokenPayload.UserId)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return c.JSON(http.StatusNotFound, NOT_FOUND)
+		}
+		return c.JSON(http.StatusInternalServerError, INTERNAL_ERROR)
+	}
+
+	hasComment, err := s.store.GetComment(c.Request().Context(), commentId)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return c.JSON(http.StatusNotFound, NOT_FOUND)
+		}
+		return c.JSON(http.StatusInternalServerError, INTERNAL_ERROR)
+	}
+
+	if userIdentityId.ID != hasComment.UserIdentityID {
+		return c.JSON(http.StatusUnauthorized, UNAUTHORIZED)
+	}
+
+	comment, err := s.store.DeleteComment(c.Request().Context(), commentId)
 	if err != nil {
 		if err == sql.ErrNoRows {
 			return c.JSON(http.StatusNotFound, NOT_FOUND)
--- handler/comments_test.go
+++ handler/comments_test.go
@@ -1,14 +1,20 @@
 package handler
 
 import (
+	"cnfs/common"
 	"cnfs/db/mock"
 	db "cnfs/db/sqlc"
 	"database/sql"
+	"fmt"
 	"net/http/httptest"
+	"reflect"
+	"strings"
 	"testing"
+	"time"
 
 	"github.com/golang/mock/gomock"
 	"github.com/google/uuid"
+	"github.com/labstack/echo/v4"
 	"github.com/stretchr/testify/require"
 )
 
@@ -75,7 +81,7 @@ func TestListAllComments(t *testing.T) {
 
 func TestGetComment(t *testing.T) {
 	post := RandomPost(t, uuid.New())
-	comment := RandomComment(t, post.ID, uuid.NullUUID{})
+	comment := RandomComment(t, post.ID, uuid.Nil)
 
 	testCases := []testCase{
 		{
@@ -127,6 +133,280 @@ func TestGetComment(t *testing.T) {
 
 			rec := httptest.NewRecorder()
 			req := httptest.NewRequest("GET", tc.payload, nil)
+
+			server.router.ServeHTTP(rec, req)
+			tc.checkResponse(rec)
+		})
+	}
+}
+
+type eqCreateCommentParams struct {
+	arg db.CreateCommentParams
+	ID  uuid.UUID
+}
+
+func (e *eqCreateCommentParams) Matches(x interface{}) bool {
+	arg, ok := x.(db.CreateCommentParams)
+	if !ok {
+		return false
+	}
+
+	arg.ID = e.ID
+	arg = e.arg
+	return reflect.DeepEqual(e.arg, arg)
+}
+
+func (e *eqCreateCommentParams) String() string {
+	return fmt.Sprintf("is equal to %v", e.arg)
+}
+
+func EqCreateCommentParams(arg *db.CreateCommentParams, id uuid.UUID) gomock.Matcher {
+	return &eqCreateCommentParams{arg: *arg, ID: id}
+}
+
+func TestCreateComment(t *testing.T) {
+	_, user := RandomUser(t)
+	post := RandomPost(t, uuid.New())
+	comment := RandomComment(t, post.ID, uuid.Nil)
+	arg := db.CreateCommentParams{
+		ID:             comment.ID,
+		PostID:         post.ID,
+		Content:        comment.Content,
+		UserIdentityID: comment.UserIdentityID,
+		ParentID:       comment.ParentID,
+		CreatedAt:      comment.CreatedAt,
+		UpdatedAt:      comment.UpdatedAt,
+	}
+
+	testCases := []testCase{
+		{
+			name:    "OK",
+			payload: fmt.Sprintf(`{"user_identity_id": %q, "post_id": %q, "parent_id": %q, "content": %q}`, comment.UserIdentityID, post.ID, comment.ParentID, comment.Content),
+			buildStubs: func(store *mock.MockStore) {
+				store.EXPECT().CreateComment(gomock.Any(), EqCreateCommentParams(&arg, arg.ID)).Times(1).Return(comment, nil)
+			},
+			checkResponse: func(rec *httptest.ResponseRecorder) {
+				require.Equal(t, 200, rec.Code)
+			},
+		},
+		{
+			name:    "404 post not found",
+			payload: fmt.Sprintf(`{"user_identity_id": %q, "post_id": %q, "parent_id": %q, "content": %q}`, comment.UserIdentityID, post.ID, comment.ParentID, comment.Content),
+
+			buildStubs: func(store *mock.MockStore) {
+				store.EXPECT().CreateComment(gomock.Any(), EqCreateCommentParams(&arg, arg.ID)).Times(1).Return(db.Comment{}, sql.ErrNoRows)
+			},
+			checkResponse: func(rec *httptest.ResponseRecorder) {
+				require.Equal(t, 404, rec.Code)
+			},
+		},
+		{
+			name:    "500 internal server error",
+			payload: fmt.Sprintf(`{"user_identity_id": %q, "post_id": %q, "parent_id": %q, "content": %q}`, comment.UserIdentityID, post.ID, comment.ParentID, comment.Content),
+
+			buildStubs: func(store *mock.MockStore) {
+				store.EXPECT().CreateComment(gomock.Any(), EqCreateCommentParams(&arg, arg.ID)).Times(1).Return(db.Comment{}, sql.ErrConnDone)
+			},
+			checkResponse: func(rec *httptest.ResponseRecorder) {
+				require.Equal(t, 500, rec.Code)
+			},
+		},
+	}
+
+	for i := range testCases {
+		tc := testCases[i]
+
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			store := mock.NewMockStore(ctrl)
+			tc.buildStubs(store)
+
+			server, err := NewServer(store, cfg)
+			require.NoError(t, err)
+
+			rec := httptest.NewRecorder()
+			req := httptest.NewRequest("POST", "/api/v1/comments", strings.NewReader(tc.payload))
+			req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+			tokenPayload, _, err := server.tokenMaker.CreateToken(user.ID, user.Username, cfg.AccessTokenDuration)
+			require.NoError(t, err)
+			req.Header.Set("Authorization", "Bearer "+tokenPayload)
+
+			server.router.ServeHTTP(rec, req)
+			tc.checkResponse(rec)
+		})
+	}
+}
+
+type eqUpdateCommentParams struct {
+	arg       db.UpdateCommentParams
+	updatedAt time.Time
+}
+
+func (e *eqUpdateCommentParams) Matches(x interface{}) bool {
+	arg, ok := x.(db.UpdateCommentParams)
+	if !ok {
+		return false
+	}
+
+	arg.UpdatedAt = e.updatedAt
+	// arg = e.arg
+	return reflect.DeepEqual(e.arg, arg)
+}
+
+func (e *eqUpdateCommentParams) String() string {
+	return fmt.Sprintf("is equal to %v", e.arg)
+}
+
+func EqUpdateCommentParams(arg db.UpdateCommentParams, updatedAt time.Time) gomock.Matcher {
+	return &eqUpdateCommentParams{arg: arg, updatedAt: updatedAt}
+}
+
+func TestUpdateComment(t *testing.T) {
+	_, user := RandomUser(t)
+	post := RandomPost(t, uuid.New())
+	comment := RandomComment(t, post.ID, uuid.Nil)
+	newContent := common.RandomString(36)
+
+	testCases := []testCase{
+		{
+			name:    "OK",
+			payload: fmt.Sprintf(`{"content": %q}`, newContent),
+			buildStubs: func(store *mock.MockStore) {
+				updatedAt := time.Now()
+				arg := db.UpdateCommentParams{
+					UpdatedAt: updatedAt,
+					Content:   newContent,
+					ID:        comment.ID,
+				}
+
+				store.EXPECT().GetUserIdentityByUserId(gomock.Any(), gomock.Eq(user.ID)).Times(1)
+				store.EXPECT().GetComment(gomock.Any(), gomock.Eq(comment.ID)).Times(1)
+				comment.UpdatedAt = updatedAt
+				store.EXPECT().UpdateComment(gomock.Any(), EqUpdateCommentParams(arg, updatedAt)).Times(1).Return(comment.ID, nil)
+			},
+			checkResponse: func(rec *httptest.ResponseRecorder) {
+				require.Equal(t, 200, rec.Code)
+			},
+		},
+		{
+			name:    "400 bad request - Missing payload",
+			payload: "",
+			buildStubs: func(store *mock.MockStore) {
+				store.EXPECT().GetUserIdentityByUserId(gomock.Any(), gomock.Any()).Times(0)
+			},
+			checkResponse: func(rec *httptest.ResponseRecorder) {
+				require.Equal(t, 400, rec.Code)
+			},
+		},
+		{
+			name:    "Unauthorized - Cannot update a comment that does not belong to the user",
+			payload: `{"content": "new content"}`,
+			buildStubs: func(store *mock.MockStore) {
+				store.EXPECT().GetUserIdentityByUserId(gomock.Any(), gomock.Eq(user.ID)).Times(1).Return(db.UserIdentity{
+					ID:           uuid.New(),
+					UserID:       uuid.New(),
+					IdentityHash: uuid.New(),
+				}, nil)
+				store.EXPECT().GetComment(gomock.Any(), gomock.Eq(comment.ID)).Times(1).Return(comment, nil)
+				store.EXPECT().UpdateComment(gomock.Any(), gomock.Any()).Times(0)
+			},
+			checkResponse: func(rec *httptest.ResponseRecorder) {
+				require.Equal(t, 401, rec.Code)
+			},
+		},
+	}
+
+	for i := range testCases {
+		tc := testCases[i]
+
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			store := mock.NewMockStore(ctrl)
+			tc.buildStubs(store)
+
+			server, err := NewServer(store, cfg)
+			require.NoError(t, err)
+
+			rec := httptest.NewRecorder()
+			req := httptest.NewRequest("PUT", "/api/v1/comments/"+comment.ID.String(), strings.NewReader(tc.payload))
+			req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+			tokenPayload, _, err := server.tokenMaker.CreateToken(user.ID, user.Username, cfg.AccessTokenDuration)
+			require.NoError(t, err)
+			req.Header.Set("Authorization", "Bearer "+tokenPayload)
+			req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+
+			server.router.ServeHTTP(rec, req)
+			tc.checkResponse(rec)
+		})
+	}
+}
+
+func TestDeleteComment(t *testing.T) {
+	_, user := RandomUser(t)
+	post := RandomPost(t, uuid.New())
+	comment := RandomComment(t, post.ID, uuid.Nil)
+
+	testCases := []testCase{
+		{
+			name:    "OK",
+			payload: comment.ID.String(),
+			buildStubs: func(store *mock.MockStore) {
+				store.EXPECT().GetUserIdentityByUserId(gomock.Any(), gomock.Eq(user.ID)).Times(1)
+				store.EXPECT().GetComment(gomock.Any(), gomock.Eq(comment.ID)).Times(1)
+				store.EXPECT().DeleteComment(gomock.Any(), gomock.Eq(comment.ID)).Times(1).Return(comment.ID, nil)
+			},
+			checkResponse: func(rec *httptest.ResponseRecorder) {
+				require.Equal(t, 200, rec.Code)
+			},
+		},
+		{
+			name:    "Unauthorized - Cannot delete a comment that does not belong to the user",
+			payload: comment.ID.String(),
+			buildStubs: func(store *mock.MockStore) {
+				store.EXPECT().GetUserIdentityByUserId(gomock.Any(), gomock.Eq(user.ID)).Times(1).Return(db.UserIdentity{
+					ID:           uuid.New(),
+					UserID:       uuid.New(),
+					IdentityHash: uuid.New(),
+				}, nil)
+				store.EXPECT().GetComment(gomock.Any(), gomock.Eq(comment.ID)).Times(1).Return(comment, nil)
+				store.EXPECT().DeleteComment(gomock.Any(), gomock.Any()).Times(0)
+			},
+			checkResponse: func(rec *httptest.ResponseRecorder) {
+				require.Equal(t, 401, rec.Code)
+			},
+		},
+	}
+
+	for i := range testCases {
+		tc := testCases[i]
+
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			store := mock.NewMockStore(ctrl)
+			tc.buildStubs(store)
+
+			server, err := NewServer(store, cfg)
+			require.NoError(t, err)
+
+			rec := httptest.NewRecorder()
+			req := httptest.NewRequest("DELETE", "/api/v1/comments/"+tc.payload, nil)
+			req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+			tokenPayload, _, err := server.tokenMaker.CreateToken(user.ID, user.Username, cfg.AccessTokenDuration)
+			require.NoError(t, err)
+			req.Header.Set("Authorization", "Bearer "+tokenPayload)
+			req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
 
 			server.router.ServeHTTP(rec, req)
 			tc.checkResponse(rec)
