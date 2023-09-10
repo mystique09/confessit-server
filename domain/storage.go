88393b813d7a8a790b0b791b970d389cdc486089
mystique09 <knightkrypto7@gmail.com>
2023-09-10 22:10:57 +0800

    add: storage domain

--- domain/storage.go
+++ domain/storage.go
@@ -7,12 +7,12 @@ type CacheInstance = interface{}
 type FileStorageInstance = interface{}
 
 type IDataStorage interface {
-  GetCacheInstance() IStorage[DatabaseInstance]
-  GetDatabaseInstance() IStorage[CacheInstance]
-  GetFileStorageInstance() IStorage[FileStorageInstance]
+	GetCacheInstance() IStorage[DatabaseInstance]
+	GetDatabaseInstance() IStorage[CacheInstance]
+	GetFileStorageInstance() IStorage[FileStorageInstance]
 }
 
 type IStorage[T any] interface {
-  Connect() (T, error)
-  Disconnect() error
+	Connect() (T, error)
+	Disconnect() error
 }
