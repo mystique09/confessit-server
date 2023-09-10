9f02ccab6e6cac3b459fe57cc5ced7672c0e8d45
mystique09 <knightkrypto7@gmail.com>
2023-09-10 19:48:53 +0800

    add: storage domain

---
+++ domain/storage.go
@@ -0,0 +1,18 @@
+package domain
+
+import "database/sql"
+
+type DatabaseInstance = sql.DB
+type CacheInstance = interface{}
+type FileStorageInstance = interface{}
+
+type IDataStorage interface {
+  GetCacheInstance() IStorage[DatabaseInstance]
+  GetDatabaseInstance() IStorage[CacheInstance]
+  GetFileStorageInstance() IStorage[FileStorageInstance]
+}
+
+type IStorage[T any] interface {
+  Connect() (T, error)
+  Disconnect() error
+}
