c963f925867636dd0f3e43f5cc40b2c9df191f1a
mystique09 <knightkrypto7@gmail.com>
2023-09-16 17:36:19 +0800

    feat: config loader lmao

--- config/config.go
+++ config/config.go
@@ -1,41 +1,126 @@
 package config
 
 import (
-	"log"
+	"cnfs/domain"
 	"time"
 
 	"github.com/spf13/viper"
 )
 
-type Config struct {
-	DatabaseUrl           string        `mapstructure:"DATABASE_URL"`
-	Host                  string        `mapstructure:"HOST"`
-	Port                  string        `mapstructure:"PORT"`
-	PasetoSymmetricKey    string        `mapstructure:"PASETO_SYMMETRIC_KEY"`
-	AccessTokenSecretKey  string        `mapstructure:"ACCESS_TOKEN_SECRET"`
-	AccessTokenDuration   time.Duration `mapstructure:"ACCESS_TOKEN_DURATION"`
-	RefreshTokenSecretKey string        `mapstructure:"REFRESH_TOKEN_SECRET"`
-	RefreshTokenDuration  time.Duration `mapstructure:"REFRESH_TOKEN_DURATION"`
-	ClientUrl             string        `mapstructure:"CLIENT_URL"`
+type configLoader struct {}
+
+func NewConfigLoader() domain.IConfigLoader {
+  return configLoader{}
 }
 
-func LoadConfig(path, name string) (Config, error) {
-	viper.AddConfigPath(path)
-	viper.SetConfigName(name)
-	viper.SetConfigType("env")
-
-	viper.AutomaticEnv()
-
-	if err := viper.ReadInConfig(); err != nil {
-		return Config{}, err
-	}
-
-	var config Config
-	if err := viper.Unmarshal(&config); err != nil {
-		return Config{}, nil
-	}
-
-	log.Println(config)
-
-	return config, nil
+func (loader configLoader) Unmarshal(cfg interface{}) error {
+  return viper.Unmarshal(&cfg)
 }
+
+type config struct {
+  server domain.IServerConfig
+  token domain.ITokenConfig
+}
+
+func NewConfig(server domain.IServerConfig, token domain.ITokenConfig) domain.IConfig {
+  return config{
+    server: server,
+    token: token,
+  }
+}
+
+func (cfg config) ServerConfig() domain.IServerConfig {
+  return cfg.server
+}
+
+func (cfg config) TokenConfig() domain.ITokenConfig {
+  return cfg.token
+}
+
+type serverConfig struct {
+  host string `mapstructure:"HOST"`
+  port string `mapstructure:"PORT"`
+  databaseUrl string `mapstructure:"DATABASE_URL"`
+  clientUrl string `mapstructure:"CLIENT_URL"`
+}
+
+func NewServerConfig(loader domain.IConfigLoader) (domain.IServerConfig, error) {
+  var cfg serverConfig
+  if err := loader.Unmarshal(&cfg); err != nil {
+    return serverConfig{}, err
+  }
+  return cfg, nil
+}
+
+func (cfg serverConfig) Host() string {
+  return cfg.host
+}
+
+func (cfg serverConfig) Port() string {
+  return cfg.port
+}
+
+func (cfg serverConfig) DatabaseUrl() string {
+  return cfg.databaseUrl
+}
+
+func (cfg serverConfig) ClientUrl() string {
+  return cfg.clientUrl
+}
+
+type tokenConfig struct {
+  authSecretKey string `mapstructure:"PASETO_SYMMETRIC_KEY"`
+  accessTokenSecretKey string `mapstructure:"ACCESS_TOKEN_SECRET"`
+  accessTokenDuration time.Duration `mapstructure:"ACCESS_TOKEN_DURATION"`
+  refreshTokenSecretKey string `mapstructure:"REFRESH_TOKEN_SECRET"`
+  refreshTokenDuration time.Duration `mapstructure:"REFRESH_TOKEN_DURATION"`
+}
+
+func NewTokenConfig(loader domain.IConfigLoader) (domain.ITokenConfig, error) {
+  var cfg tokenConfig
+  if err := loader.Unmarshal(&cfg); err != nil {
+    return tokenConfig{}, err
+  }
+  return cfg, nil
+}
+
+func (cfg tokenConfig) AuthSecretKey() string {
+  return cfg.authSecretKey
+}
+
+func (cfg tokenConfig) AccessTokenSecretKey() string {
+  return cfg.accessTokenSecretKey
+}
+
+func(cfg tokenConfig) AccessTokenDuration() time.Duration {
+  return cfg.accessTokenDuration
+}
+
+func (cfg tokenConfig) RefreshTokenSecretKey() string {
+  return cfg.refreshTokenSecretKey
+}
+
+func (cfg tokenConfig) RefreshTokenDuration() time.Duration {
+  return cfg.refreshTokenDuration
+}
+
+// func LoadConfig(path, name string) (Config, error) {
+// 	viper.AddConfigPath(path)
+// 	viper.SetConfigName(name)
+// 	viper.SetConfigType("env")
+
+// 	viper.AutomaticEnv()
+
+// 	if err := viper.ReadInConfig(); err != nil {
+// 		return Config{}, err
+// 	}
+
+// 	var config Config
+// 	if err := viper.Unmarshal(&config); err != nil {
+// 		return Config{}, nil
+// 	}
+
+// 	log.Println(config)
+
+// 	return config, nil
+// }
